bd95be9461dc7c2b67ae91322edd2942f6a7d16e
neelap <neela_reddy@outlook.com>
2016-04-11 03:51:51 +0530

    commit Jersey Verticle

--- src/main/java/com/nems/common/JerseyVerticle.java
+++ src/main/java/com/nems/common/JerseyVerticle.java
@@ -1,23 +1,47 @@
 package com.nems.common;
 
 import com.englishtown.vertx.hk2.BootstrapBinder;
+import com.englishtown.vertx.hk2.HK2VerticleFactory;
 import io.vertx.core.*;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.spi.VerticleFactory;
+import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by NE281900 on 4/9/2016.
  */
-public class JerseyVerticle extends AbstractVerticle {
-    @Override
-    public void init(Vertx vertx, Context context) {
-        super.init(vertx, context);
-    }
+public class JerseyVerticle {
 
-    @Override
-    public void start(Future<Void> startFuture) throws Exception {
+    private final static Logger logger = LoggerFactory.getLogger(JerseyVerticle.class);
+
+    public static void main(String[] args) throws Exception {
+
+        Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(
+                new DropwizardMetricsOptions().setEnabled(true).setJmxEnabled(true).setJmxDomain("nems-metrics")
+        ));
+
+        HttpServer httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+
+        Optional<VerticleFactory> factory = vertx.verticleFactories()
+                .stream()
+                .filter(f -> f instanceof HK2VerticleFactory)
+                .findFirst();
+
+        if (!factory.isPresent()) {
+            throw new IllegalStateException("The HK2Verticle factory is not registered");
+        }
+
         JsonObject config = new JsonObject()
                 .put("hk2_binder", new JsonArray().add(BootstrapBinder.class.getCanonicalName()))
                 .put("jersey", new JsonObject()
@@ -32,18 +56,24 @@ public class JerseyVerticle extends AbstractVerticle {
         DeploymentOptions options = new DeploymentOptions().setConfig(config);
         options.setWorker(true);
 
-        vertx.deployVerticle("java-hk2:" + JerseyVerticle.class.getCanonicalName(), options, result -> {
+        vertx.deployVerticle("java-hk2:" + com.englishtown.vertx.jersey.JerseyVerticle.class.getCanonicalName(), options, result -> {
             if (result.succeeded()) {
-                System.out.println("jersey Deployed");
                 future.complete(result.result());
             } else {
                 future.completeExceptionally(result.cause());
             }
         });
-    }
 
-    @Override
-    public void stop(Future<Void> stopFuture) throws Exception {
-        super.stop(stopFuture);
+        try {
+            String deploymentID = future.get();
+            logger.info("Deployed jersey verticle: " + deploymentID);
+
+        } catch (Throwable t) {
+            logger.error("Error deploying verticle: " + t.getMessage());
+            t.printStackTrace();
+        }
+
+        httpServer.requestHandler(router::accept).listen(9000);
+
     }
 }
